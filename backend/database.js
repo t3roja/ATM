58473f932642f7226da71c6ee4e843e9da785b73
t3roja <ronkainen@hotmail.fi>
2024-04-12 14:47:48 +0300

    alku

--- backend/database.js
+++ backend/database.js
@@ -6,13 +6,3 @@ const connection = mysql.createPool({
   database: 'bank_automat'
 });
 module.exports = connection;
-/*
-const mysql = require('mysql2');
-const connection = mysql.createPool({
-  host: 'my-sql-atm-test.a.aivencloud.com',
-  user: 'avnadmin',
-  password: 'AVNS_HVZe7dIzEagkrFXRugn',
-  database: 'bank_automat',
-  port: '15120',
-});
-module.exports = connection;*/
