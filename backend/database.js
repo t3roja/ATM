9e7596b3ebb10a6596c53d94448188e6e56acc3b
t3roja <ronkainen@hotmail.fi>
2024-04-12 17:03:54 +0300

    päivitystä

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,19 @@
-const mysql = require('mysql2');
+/*const mysql = require('mysql2');
 const connection = mysql.createPool({
   host: '127.0.0.1',
   user: 'netuser',
   password: 'netpass',
   database: 'bank_automat'
 });
-module.exports = connection;
+module.exports = connection;*/
+
+const mysql=require('mysql2');
+const dotenv=require('dotenv');
+dotenv.config();
+
+//const conn="mysql://netuser:netpass@127.0.0.1:3306/bank_automat";
+
+const connection=mysql.createPool(process.env.MySQL_DB);
+//const connection=mysql.createPool(conn);
+
+module.exports=connection;
