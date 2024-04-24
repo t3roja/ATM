bf5294054b53c344d01e98661aec81383c8a7872
t3roja <ronkainen@hotmail.fi>
2024-04-24 19:44:11 +0300

    päivitystä kaikkeen

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,18 @@
-const mysql = require('mysql2');
+/*const mysql = require('mysql2');
 const connection = mysql.createPool({
   host: '127.0.0.1',
   user: 'netuser',
   password: 'netpass',
   database: 'bank_automat'
 });
-module.exports = connection;
+module.exports = connection;*/
+
+const mysql=require('mysql2');
+const dotenv=require('dotenv');
+dotenv.config();
+
+const conn="mysql://avnadmin:AVNS_LboqcJSqlpoD97kgefH@my-sql-atm-test.a.aivencloud.com:15120/bank_automat";
+
+const connection=mysql.createPool(conn);
+
+module.exports=connection;
